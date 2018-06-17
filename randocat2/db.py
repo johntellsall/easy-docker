873f37dc447e3dc6a88eb35cdab1cc86c3ddc690
John Mitchell <john@johntellsall.com>
2018-06-17 12:35:12 -0700

    wip: Randocat Pyramid w/ SQLA

---
+++ randocat2/db.py
@@ -0,0 +1,34 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+
+metadata = MetaData()
+books = Table('book', metadata,
+  Column('id', Integer, primary_key=True),
+  Column('title', String),
+  Column('primary_author', String),
+)
+
+# import ipdb ; ipdb.set_trace()
+engine = create_engine('sqlite:///bookstore.db')
+metadata.create_all(engine)
+
+from sqlalchemy.sql import text
+with engine.connect() as con:
+
+    data = ( { "id": 1, "title": "The Hobbit", "primary_author": "Tolkien" },
+             { "id": 2, "title": "The Silmarillion", "primary_author": "Tolkien" },
+    )
+
+    statement = text("""INSERT INTO book(id, title, primary_author) VALUES(:id, :title, :primary_author)""")
+
+    for line in data:
+        con.execute(statement, **line)
+        
+with engine.connect() as con:
+
+    rs = con.execute('SELECT * FROM book')
+
+    for row in rs:
+        print(row)
