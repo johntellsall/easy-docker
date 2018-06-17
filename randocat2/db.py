efcbab040f820fe8fe8a81d49ee47021b47f4a23
John Mitchell <john@johntellsall.com>
2018-06-17 12:39:27 -0700

    works w/ Postgres

--- randocat2/db.py
+++ randocat2/db.py
@@ -1,7 +1,5 @@
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
 
 metadata = MetaData()
 books = Table('book', metadata,
@@ -10,22 +8,21 @@ books = Table('book', metadata,
   Column('primary_author', String),
 )
 
-import ipdb ; ipdb.set_trace()
-# engine = create_engine('sqlite:///bookstore.db')
 engine = create_engine('postgresql://postgres:@database/postgres')
 metadata.create_all(engine)
 
-from sqlalchemy.sql import text
-with engine.connect() as con:
+if 0:
+	from sqlalchemy.sql import text
+	with engine.connect() as con:
 
-    data = ( { "id": 1, "title": "The Hobbit", "primary_author": "Tolkien" },
-             { "id": 2, "title": "The Silmarillion", "primary_author": "Tolkien" },
-    )
+	    data = ( { "id": 1, "title": "The Hobbit", "primary_author": "Tolkien" },
+	             { "id": 2, "title": "The Silmarillion", "primary_author": "Tolkien" },
+	    )
 
-    statement = text("""INSERT INTO book(id, title, primary_author) VALUES(:id, :title, :primary_author)""")
+	    statement = text("""INSERT INTO book(id, title, primary_author) VALUES(:id, :title, :primary_author)""")
 
-    for line in data:
-        con.execute(statement, **line)
+	    for line in data:
+	        con.execute(statement, **line)
 
 with engine.connect() as con:
 
